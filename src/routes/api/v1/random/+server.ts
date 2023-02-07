da2dca8c8ba48d86d47bccc685e09a9ebd5a35b6
Alexander Liu <a@alexanderliu.com>
2023-02-07 15:53:02 -0800

    feat: pham api v1

---
+++ src/routes/api/v1/random/+server.ts
@@ -0,0 +1,14 @@
+import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
+import { BASE_URL } from '$env/static/private';
+const photos = import.meta.glob('$/static/photos/*.*');
+
+export const GET = (() => {
+	const photoPaths = Object.keys(photos);
+	const random = Math.floor(Math.random() * photoPaths.length);
+	const randomPhoto = photoPaths[random].substring(7);
+
+	return json({
+		src: new URL(randomPhoto, BASE_URL).toString()
+	});
+}) satisfies RequestHandler;
